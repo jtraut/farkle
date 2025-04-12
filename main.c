963d193dfe8f42d5914e9796d127f3a9ce3f04ae
jtraut <jaketraut@gmail.com>
2025-04-11 22:12:36 -0600

    vs code c program wip

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	printf("FARKLE DICE GAME\n");
+
+	// TODO: various classes for...
+	// dice, rolling (random num generating), directions, scorekeeping, players
+
+	return 0;
+}
